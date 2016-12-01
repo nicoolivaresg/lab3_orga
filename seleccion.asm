b9300702a59a1f5335272f0c3a5a2db8ba961569
Nicolas O <nico.olivaresg@hotmail.cl>
2016-12-01 11:33:36 -0300

    Lectura semilista, aun falta lectura del primer numero, Lista enlazada con mostrar_lista, es_vacia, insertar_inicio

--- seleccion.asm
+++ seleccion.asm
@@ -14,8 +14,8 @@
 		
 		move $s7, $a1
 		move $s6, $a0
-		lw $s5, 0($a1)
-		lw $s7, 4($a1)
+		#lw $s5, 0($a1)
+		#lw $s7, 4($a1)
 		addi $t0, $zero, 2
 		#Se verifica que el número de argumentos es el correcto
 		#bne $s6, $t0, fallo_entrada_argumentos
@@ -28,8 +28,8 @@
 			###############################################################
   				# Open (for writing) a file that does not exist
   				li   $v0, 13       # system call for open file
-  				la   $a0, ($s5)     # output file name
-  				#la $a0, file
+  				#la   $a0, ($s5)     # output file name
+  				la $a0, file
   				li   $a1, 0        # Open for writing (flags are 0: read, 1: write)
   				li   $a2, 0        # mode is ignored
   				syscall            # open a file (file descriptor returned in $v0)
@@ -42,36 +42,33 @@
 				li   $a2, 1048576    # hardcoded buffer length
 				syscall            # read to file
 				move $s2, $v0
-				
+				addi $s2, $s2,-2
 				la $s1, buffer	 #$s1 = direccion de buffer, todas las lineas del archivo
-				
-				subi $s2, $s2,2		
-				add $t1,$zero,$s2
-				addi $t6,$zero,0
-				addi $t5,$zero,0
+				addi $s6,$zero,0
+				addi $s5,$zero,0
+				add $s4, $zero,$zero #puntero a cabeza de la lista L=NULL
 				loop_lectura:
 					
-					add $t4, $s1,$s2 # obtencion de direccion de caracter
-					lb $t3,($t4) #obtencion de numero ascii de caracter desde la direccion
+					add $t9, $s2,$s1 # obtencion de direccion de caracter
+					lb $s3,($t9) #obtencion de numero ascii de caracter desde la direccion
 					
-					beq  $t3,45,signo_menos # pregunta si existe un signo menos o negativo
+					beq  $s3,45,signo_menos # pregunta si existe un signo menos o negativo
 					volver_signo_negativo:
-					beq $t3,10,encontro_salto # pregunta si existe un salto de linea
+					beq $s3,10,encontro_salto # pregunta si existe un salto de linea
 					volver_encontro_salto:
 					
 					#convertir caracter a digito decimal												
-					add $a1, $zero, $t3
+					add $a1, $zero, $s3
   					jal character_to_int
-  					move $t7, $v0
+  					move $s7, $v0
   					#multiplicar digito decimal por la potencia correspondiente de 10
   					addi $a0, $zero, 10 ##Argumento base
-					add $a1, $zero, $t5 ## Argumento exponente
+					add $a1, $zero, $s5 ## Argumento exponente
 					jal pow
-					move $t3,$v1 ##Resultado de potencia
-					mul $t3,$t3,$t7 # (DIGITO x 10 ^ potencia)
-					
-					add $t6,$t6,$t3  #(DIGITO x 10 ^ potencia) + digito anterior
-					addi $t5,$t5,1 					
+					move $s3,$v1 ##Resultado de potencia
+					mul $s3,$s3,$s7 # (DIGITO x 10 ^ potencia)
+					add $s6,$s6,$s3  #(DIGITO x 10 ^ potencia) + lo anterior
+					addi $s5,$s5,1 					
 					beq $s2,0, end_loop_lectura
 					subi $s2,$s2,1 
 					b loop_lectura					
@@ -89,7 +86,7 @@
 					
 					
 				numero_negativo:
-					sub $t6, $zero, $t6
+					sub $s6, $zero, $s6
 					addi $k0, $zero, 0
 					b volver_numero_negativo
 				
@@ -100,20 +97,38 @@
 					
 				agregar_lista:
 					##ACA DEBO METER EL NUMERO RECIENTEMENTE CREADO EN LA LISTA ENLAZADA
-					li $v0,1
-					move $a0, $t6
-					syscall
-					li $v0,4
-					la $a0, salto_linea
-					syscall
-					addi $t6,$zero,0 #reiniciar suma del numero total
-					addi $t5 $zero,0
+					add $a0, $zero, $s4
+					add $a1, $zero, $s6
+					jal insertar_inicio
+					move $s4,$v0
+					#li $v0,1
+					#move $a0, $s6
+					#syscall
+					#li $v0,4
+					#la $a0, salto_linea
+					#syscall
+					addi $s6,$zero,0 #reiniciar suma del numero total
+					addi $s5 $zero,0
 					subi $s2,$s2,1
 					beq $s2,0, end_loop_lectura
 					
 					b loop_lectura
 					
 				end_loop_lectura:
+				move $s0, $zero
+				move $s1, $zero
+				move $s2, $zero
+				move $s3, $zero
+				move $s5, $zero
+				move $s6, $zero
+				move $s7, $zero
+				move $t9, $zero
+				move $k0, $zero
+				move $k1, $zero
+				
+				add $a0, $zero, $s4
+				jal mostrar_lista
+				
 				###############################################################
 				# Close the file 
 				li   $v0, 16       # system call for close file
@@ -154,7 +169,7 @@
 		syscall
 		jr $ra
 
-	#Este procedimiento se encarga de convertir un string que representa a un número, en un entero
+	
 
 	#Este procedimiento se encarga de convertir un caracter a su representacion decimal
 	#Entrada: En $a1 un valor de caracter en código ASCII
@@ -163,9 +178,9 @@
 		blt $a1,48, error_character_to_int
 		bgt $a1,57,error_character_to_int
 		addi $t0,$zero,47
-		addi $t1, $zero, -1
+		addi $v0, $zero, -1
 		loop_character_to_int:
-			addi $t1,$t1,1
+			addi $v0,$v0,1
 			addi $t0,$t0,1
 			bne $a1,$t0,loop_character_to_int
 			j end_loop_character_to_int
@@ -178,7 +193,6 @@
 			syscall
 			b end	
 		end_loop_character_to_int:
-			add $v0, $zero, $t1
 			jr $ra
 	
 	#Este procedimiento se encarga de obtener la potencia de b^e
@@ -199,3 +213,98 @@
 			jr $ra
 		primer_pow:
 			b volver_primer_pow
+			
+			
+			
+	#Este procedidmiento se encarga de crear un nuevo nodo con un dato
+	#Entrada: $a0-> dato entero para el nodo
+	#Salida: $v0-> Direccion de memoria del nodo
+	crear_nodo:
+		addi $sp, $sp, -4
+		sw $a0, 0($sp)
+		addi $v0,$zero,9
+		addi $a0,$zero,8
+		syscall
+		lw $a0, 0($sp)
+		sw $a0, 0($v0) #Nodo->dato = dato_argumento
+		sw $zero, 4($v0)#Nodo->sig = null o 0 o zero
+		addi $sp,$sp,4
+		jr $ra
+	
+	#Este procedimiento se encarga de crear una lista enlazada vacia
+	#Entrada: Sin entrada
+	#Salida: $v0-> direccion a la lista creada
+	crear_lista:
+		addi $v0,$zero,9
+		addi $a0,$zero,8
+		syscall
+		sw $zero, 0($v0)
+		sw $zero, 4($v0)
+		jr $ra
+		
+	#Este procedimiento se encarga de reconocer si una lista está vacia
+	#Entrada: $a0-> direccion de memoria de la lista
+	#Salida: $v0-> un valor 1 o 0. 1 si esta vacia 0 si no esta vacia
+	es_vacia:
+		beq $a0, $zero, lista_vacia
+		addi $v0,$zero, 0
+		jr $ra
+		lista_vacia:
+			addi $v0, $zero, 1
+			jr $ra
+		
+		
+	#Este procedimiento se encarga de insertar un elemento al INICIO de una lista
+	#Entrada: 	$a0-> direccion de memoria de la lista
+	#	  	$a1 -> dato a guardar
+	#Salida: 	$v0-> nueva direccion de memoria de la lista
+	insertar_inicio:
+		addi $sp, $sp, -12
+		sw $ra, 0($sp)
+		sw $a0, 4($sp)
+		sw $a1, 8($sp)
+		jal es_vacia
+		beq $v0, 1, lista_vacia_insertar_inicio
+		##lista NO vacia
+			add $a0, $zero, $a1
+			jal crear_nodo
+			
+			lw $a0, 4($sp) # $a0 -> puntero primer nodo
+			sw $a0, 4($v0) #nuevo->sig = puntero primer nodo			
+			b end_insertar_inicio
+		
+		lista_vacia_insertar_inicio:
+		##lista vacia
+			add $a0, $zero, $a1
+			jal crear_nodo
+			b end_insertar_inicio
+		end_insertar_inicio:
+			lw $k0, 0($sp)
+			addi $sp, $sp,12
+			jr $k0
+			
+	#Este procedimiento se encarga de mostrar por panatalla la lista enlazada
+	#Entrada:	$a0->direccion de memoria de lista
+	#Salida:	Sin salida
+	mostrar_lista:
+		addi $sp,$sp,-4
+		sw $a0, 0($sp)
+		move $t2, $a0
+		loop_mostrar_lista:
+			li $v0,1
+			lw $a0,0($t2)
+			syscall
+			li $v0,4
+			la $a0,salto_linea
+			syscall
+			lw $t0, 4($t2)
+			beq $t0,$zero,end_mostrar_lista
+			lw $t2, 4($t2)
+			b loop_mostrar_lista
+		end_mostrar_lista:
+			lw $a0, 0($sp)
+			addi $sp, $sp, 4
+			jr $ra
+			
+		
+		
