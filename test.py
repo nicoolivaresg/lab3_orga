5423ffcca515817ac3c8f66e8b116109350db972
Nicolas O <nico.olivaresg@hotmail.cl>
2016-12-10 06:26:35 -0300

    Fase de pruebas de listas enlazadas, se procede a comparar

--- test.py
+++ test.py
@@ -6,16 +6,9 @@ import os
 ################ Definicion de variables globales ###########
 NUMERO_CONJUNTOS = 0
 ID_ARCHIVO = 0
-PRUEBAS = ['secuencia({0},{1})','reverse_doble({0},{1})','primer_elemento({0},{1})','ultimo_elemento({0},{1})','reflexividad({0},{1})','transitividad({0},{1})','antisimetria({0},{1})']#,'orden_recursivo_grado_n({0})']
-COMANDOS = ['java -jar Mars_cache.jar seleccion_testing.asm pa input/CP_{0}.txt output/OP_S_{1}.txt','java -jar Mars_cache.jar mezcla_testing.asm pa input/CP_{0}.txt output/OP_M_{1}.txt']
+PRUEBAS = ['secuencia({0},{1})','reverse_doble({0},{1})','primer_elemento({0},{1})','ultimo_elemento({0},{1})','reflexividad({0},{1})','transitividad({0},{1})','antisimetria({0},{1})','orden_orden({0},{1},{2})']
+COMANDOS = ['java -jar Mars_cache.jar seleccion_testing.asm pa input/CP_{0}.txt output/OP_{1}_{2}.txt','java -jar Mars_cache.jar mezcla_testing.asm pa input/CP_{0}.txt output/OP_{1}_{2}.txt']
 ################ Definicion de funciones ####################
-
-#Funcion que toma un archivo de texto con un conjunto
-#y lo carga en una lista
-#input:  Sin entrada
-#output: Una lista de elementos enteros
-
-
 #Funcion que verifica una propiedad de una lista o conjunto ordenada con la relacion binaria <=:
 #Propiedad: a[i] <= a[i+1] con i = 0,1,2,...,n
 #input: un conjunto de elementos enteros
@@ -94,7 +87,7 @@ def reflexividad(lista,n):
     if lista:
         lista_prima = lista
         for i  in range(0,n):
-            if lista_prima[i] != lista[i]:
+            if lista_prima[i] > lista[i]:
                 return False
         return True
     else:
@@ -106,7 +99,6 @@ def reflexividad(lista,n):
 #output:  un boolean de verificacion
 def transitividad(lista,n):
     if lista:
-
         if n >= 3:
             p = random.randint(0, n-3)
             a = lista[p]
@@ -118,7 +110,10 @@ def transitividad(lista,n):
                 else:
                     return False
         else:
-            return False
+            if lista[0]> lista[1]:
+                return False
+            else:
+                return True
     else:
         return True
 
@@ -130,20 +125,40 @@ def transitividad(lista,n):
 #output:  un boolean de verificacion
 def antisimetria(lista,n):
     if lista:
-        for i  in range(0,n-1):
-            a = lista[i]
-            b = lista [i+1]
-            if a>b and b>a:
-                return False
-        return True
+        contador=0
+        for i in range(0,n):
+            a =lista[i]
+            for j in range(0,n):
+                b=lista[j]
+                if a<=b and b<=a:
+                    contador = contador+1
+        if contador < n:
+            return False
+        else:
+            return True
     else:
         return True
 
 #Funcion que se encarga de verificar si se cumple la propiedad de ordenamiento recursivo
-#input: lista -> lista de elementos
-#       n -> orden del ordenamiento
+#input: lista_ordenada -> lista de elementos ya ordendada
+#       id -> identificacion de archivo
+#       algoritmo -> nombre de algoritmo
 #output: un boolean de verificacion
-def orden_recursivo_grado_n(lista):
+def orden_orden(lista_ordenada,id, algoritmo):
+    try:
+        entrada = 'output/OP_{0}_{1}.txt'.format(algoritmo,id)
+        salida = 'output/OP_{0}_orden_orden_{1}.txt'.format(algoritmo,id)
+        comando = 'java -jar Mars_cache.jar {0}.asm pa {1} {2}'.format(algoritmo,entrada,salida)
+        os.system(comando)
+        L = leer_entrada(salida)
+        if len(L)!= len(lista_ordenada):
+            return False
+        for i in range(0,len(L)):
+            if L[i] != lista_ordenada[i]:
+                return False
+        return True
+    except Exception as e:
+        raise
     return True
 
 
@@ -153,28 +168,88 @@ def orden_recursivo_grado_n(lista):
 #       max -> máximo número en la lista
 #       n -> cantidad de numeros aleatorios por archivo
 #output:  sin salida
-def generar_entrada(min,max,n):
+def generar_entrada(min,max,n,id):
     comando = 'mkdir -p input'
     os.system(comando)
-    comando = 'rm input/*'
-    os.system(comando)
-    nombre = 'input/CP_{0}.txt'.format(ID_ARCHIVO)
+    nombre = 'input/CP_{0}.txt'.format(id)
     out = open(nombre,'w')
     for i in range (0,n):
         r = random.randint(min,max)
         out.write(str(r)+'\n')
     out.close()
 
-##Esta funcion solo es provisoria para reemplazar la lectura del archivo en mips
+#Esta funcion se encarga de leer un archivo de texto
+#con el formato de la entrada o salida y cargarla sobre una lista de Python
+#Entrada: archivo-> Nombre de archivo(ruta)
+#Salida: L-> Lista con los valores que estaban en el archivo
 def leer_entrada(archivo):
     L =[]
     file = open(archivo,'r')
     for linea in file:
-        L.append(int(linea))
+        try:
+            L.append(int(linea))
+        except Exception as e:
+            raise
     file.close()
     return L
 
+#Esta funcion se encarga de ejecutar todo el conjunto de pruebas sobre todos los programas
+#Entrada:
+#Salida: Boolean si todos los programas pasaron las pruebas
+def testing(num_pruebas, min,max,n):
+    for i in range(0,num_pruebas):
+        ########### Generar Caso de Prueba ##############
+        print "\nGenerando caso de prueba, revisar input/"
+        generar_entrada(min,max,n,i)
+        print 'CASO DE PRUEBA: {0}'.format(i)
+        for j in range(0,len(COMANDOS)):
+            ################# TESTING ##############
+            algoritmo = COMANDOS[j].split()[3].split(".")[0]
+            print "\nOrdenando con {0}... Espere...".format(algoritmo)
+            print "Entrada: {0}".format("input/CP_{0}.txt".format(i))
+            try:
+                os.system(COMANDOS[j].format(i,algoritmo,i))
+                print "Salida: {0}".format('output/OP_{0}_{1}.txt'.format(algoritmo,i))
+                pass
+            except Exception as e:
+                print "Error en ejecución de ordenamiento {0}".format(algoritmo)
+                exit()
+            ########## Cargar útlimo Caso de Prueba creado ################
+            try:
+                L = leer_entrada('output/OP_{0}_{1}.txt'.format(algoritmo,i))
+                pass
+            except Exception as e:
+                print "Error en la lectura de archivo de {0} ya ordendado ".format(algoritmo)
+                exit()
 
+            #################### PRUEBAS selection_sort ########################
+            print "\nIniciando testing sobre {0}".format(algoritmo)
+            for test in PRUEBAS:
+                if test.split("(")[0] == 'orden_orden':
+                    print "Prueba de sort(sort)... Espere..."
+                    try:
+                        resultado_test = orden_orden(L,i,algoritmo)
+                        pass
+                    except Exception as e:
+                        print "Error en durante ejecución de prueba {0}".format(test.split("(")[0])
+                        exit()
+                else:
+                    try:
+                        resultado_test = eval(test.format(L,n))
+                        pass
+                    except Exception as e:
+                        print "Error en durante ejecución de prueba {0}".format(test.split("(")[0])
+                        exit()
+
+                if resultado_test:
+                    print 'Éxito en prueba: {0}'.format(test.split("(")[0])
+                    resultado_test=False
+                else:
+                    print 'Fallo en prueba: {0}'.format(test.split("(")[0])
+                    print 'CASO FALLIDO'
+                    print L
+                    exit()
+    return True
 
 ############### Bloque Principal ################
 if len(sys.argv) == 5:
@@ -191,35 +266,8 @@ if len(sys.argv) == 5:
                 MAX = int(sys.argv[3])
                 N = int(sys.argv[4])
                 ########### Inicio de pruebas ###############
-                ID_ARCHIVO = 0 ##Reiniciar Identificadores de archivos
-                for i in range(0,TOTAL_PRUEBAS):
-                    ########### Generar Caso de Prueba ##############
-                    generar_entrada(MIN,MAX,N)
-                    print 'Lista: {0}'.format(ID_ARCHIVO)
-                    ################# APLICAR ORDENAMIENTO ##############
-                    ## ITERATIVO ##
-                    print "Ordenando con selection_sort... Espere..."
-                    os.system(COMANDOS[0].format(ID_ARCHIVO,ID_ARCHIVO))
-                    ## RECURSIVO ##
-                    print "Ordenando con merge_sort... Espere..."
-                    os.system(COMANDOS[1].format(ID_ARCHIVO,ID_ARCHIVO))
-                    ########## Cargar Caso de Prueba Creado Recien ################
-                    L_selection = leer_entrada('output/OP_S_{0}.txt'.format(ID_ARCHIVO))
-                    L_merge = leer_entrada('output/OP_M_{0}.txt'.format(ID_ARCHIVO))
-                    ################# Testing de Propiedades ###########
-                    print "Iniciando testing sobre selection_sort"
-                    for test in PRUEBAS:
-                        if eval(test.format(L_selection,N)):
-                            print 'Éxito en prueba: {0}'.format(test)
-                        else:
-                            print 'Fallo en prueba: {0}'.format(test)
-                    print "Iniciando testing sobre merge_sort"
-                    for test in PRUEBAS:
-                        if eval(test.format(L_merge,N)):
-                            print 'Éxito en prueba: {0}'.format(test)
-                        else:
-                            print 'Fallo en prueba: {0}'.format(test)
-                    ID_ARCHIVO = ID_ARCHIVO + 1 #Incrementar identificador de Caso de Prueba
+                if (testing(TOTAL_PRUEBAS,MIN,MAX,N)):
+                    print '\n\tPruebas realizadas con éxito\n\n'
             else:
                 print 'Tamaño de la lista debe ser mayor que 0'
         else:
